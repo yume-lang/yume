4bd5a747336a81e613300278c56fa2feb21d7191
Emilia Dreamer <emilia@rymiel.space>
2022-03-21 19:27:05 +0200

    separate out lexer builder and lexer

--- src/yume/lexer.cr
+++ src/yume/lexer.cr
@@ -1,42 +1,49 @@
 class Yume::Lexer(*T)
-  @matches = Array({Regex, Symbol, Proc(Regex::MatchData, Union(*T))?}).new
-  @skips = Array(Regex).new
+  class Builder(*T)
+    @matches = Array({Regex, Symbol, Proc(Regex::MatchData, Union(*T))?}).new
+    @skips = Array(Regex).new
+
+    def match(regex : Regex, result : Symbol)
+      @matches << {regex, result, nil}
+    end
+
+    def match(*symbols : Symbol)
+      symbols.each do |symbol|
+        @matches << {Regex.new(Regex.escape symbol.to_s), symbol, nil}
+      end
+    end
+
+    macro keywords(*kws)
+      {% for kw in kws %}
+        match(Regex.new(Regex.escape({{kw}}.to_s)), {{"kw_#{kw.id}".id.symbolize}})
+      {% end %}
+    end
+
+    def match(regex : Regex, symbol : Symbol, &result : Regex::MatchData -> Union(*T))
+      @matches << {regex, symbol, result}
+    end
+
+    def skip(regex : Regex)
+      @skips << regex
+    end
+
+    def lex(input : String)
+      Lexer(*T).new self, input
+    end
+  end
+
+  @rules : Builder(*T)
   getter! source : String
   getter? eof : Bool = false
   property pos = 0
 
-  def self.build(&) : self
-    ins = new
+  def self.build(&) : Builder(*T)
+    ins = Builder(*T).new
     with ins yield
     ins
   end
 
-  def match(regex : Regex, result : Symbol)
-    @matches << {regex, result, nil}
-  end
-
-  def match(*symbols : Symbol)
-    symbols.each do |symbol|
-      @matches << {Regex.new(Regex.escape symbol.to_s), symbol, nil}
-    end
-  end
-
-  macro keywords(*kws)
-    {% for kw in kws %}
-      match(Regex.new(Regex.escape({{kw}}.to_s)), {{"kw_#{kw.id}".id.symbolize}})
-    {% end %}
-  end
-
-  def match(regex : Regex, symbol : Symbol, &result : Regex::MatchData -> Union(*T))
-    @matches << {regex, symbol, result}
-  end
-
-  def skip(regex : Regex)
-    @skips << regex
-  end
-
-  def lex(input : String)
-    @source = input
+  def initialize(@rules, @source)
     @pos = 0
     @eof = false
   end
@@ -46,13 +53,14 @@ class Yume::Lexer(*T)
     while true
       s_pos = pos
       found = false
-      next if @skips.any? do |m|
+      next if @rules.@skips.any? do |m|
                 if match = m.match(source, pos, Regex::Options::ANCHORED)
                   @pos = match.end
                   true
                 end
               end
-      @matches.each do |m, sym, r|
+
+      @rules.@matches.each do |m, sym, r|
         if match = m.match(source, pos, Regex::Options::ANCHORED)
           r = r.call match if r.is_a? Proc
           ret = {sym, r, (match.begin...match.end)}
--- src/yume_cli.cr
+++ src/yume_cli.cr
@@ -3,21 +3,17 @@ require "./yume"
 require "./yume/debug_lexer_consumer"
 
 module Yume
-  lexer = LEXER
   input = File.open(ARGV[0]? || "example/test.ym", &.gets_to_end)
 
   {% if flag?("debug_lex") %}
     STDERR.puts
-    lexer.lex(input)
-    debug = DebugLexerConsumer.new lexer, STDERR
+    debug = DebugLexerConsumer.new LEXER.lex(input), STDERR
     STDERR.puts
   {% end %}
 
-  lexer.lex(input)
-  puts String.build { |sb| StyleWalker.new lexer, sb }
+  puts String.build { |sb| StyleWalker.new LEXER.lex(input), sb }
 
-  lexer.lex(input)
-  program = Parser.new(lexer).parse_program
+  program = Parser.new(LEXER.lex(input)).parse_program
 
   {% if flag?("debug_ast") %}
     PrettyPrint.format(program, STDOUT, 119)
