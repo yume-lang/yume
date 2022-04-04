d44cf23ae685c307ef2a7e5b8a803757908746cd
Emilia Dreamer <emilia@rymiel.space>
2022-04-04 21:05:50 +0300

    add some stuff which will eventually be keywords

--- src/yume.cr
+++ src/yume.cr
@@ -7,16 +7,21 @@ module Yume
   VERSION = "0.1.0"
 
   LEXER = Lexer(String, Int64).build do
-    match :fn, :let, :end, :return, :if, :then, :else, :while, :struct
+    match :fn, :let, :end, :return, :if, :then, :else, :while
+    match :struct, :ref, :self, :private
     match :__primitive__, :__varargs__, :__extern__
+
     match :"(", :")", :"[", :"]", :"<", :">"
     match /-?[0-9]+/, :_int, &.[0].to_i64
+
     match :"==", :"!=", :+, :"-", :"%", :"//", :"/", :"*"
     match :"=", :",", :":", :"."
+
     match /[a-z_][a-zA-Z0-9_]*/, :_word, &.[0]
     match /[A-Z][a-zA-Z0-9]*/, :_uword, &.[0]
     match /"([^"]*)"/, :_str, &.[1]
     match /\?(\\.|[^\\])/, :_chr, &.[1]
+
     match /[\n;]/, :sep
     match /$/, :eos
     skip /[^\S\r\n]/
