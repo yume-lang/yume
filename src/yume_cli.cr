030fdddaf4b360636e5837a4b275eada533ead48
Emilia Dreamer <emilia@rymiel.space>
2022-04-05 15:37:54 +0300

    common "stdlib" automatically added

--- src/yume_cli.cr
+++ src/yume_cli.cr
@@ -3,24 +3,35 @@ require "./yume"
 require "./yume/debug_lexer_consumer"
 
 module Yume
-  input = File.open(ARGV[0]? || "example/test.ym", &.gets_to_end)
+  # TODO: more proper include logic and stdlib/prelude logic
+  source_files = [
+    "#{ {{ __DIR__ }} }/../std/core.ym",
+    ARGV[0]
+  ]
+  source_programs = source_files.map do |s|
+    input = File.open(s, &.gets_to_end)
 
-  {% if flag?("debug_lex") %}
-    STDERR.puts
-    debug = DebugLexerConsumer.new LEXER.lex(input), STDERR
-    STDERR.puts
-  {% end %}
+    puts s
 
-  puts String.build { |sb| StyleWalker.new LEXER.lex(input), sb }
+    {% if flag?("debug_lex") %}
+      STDERR.puts
+      debug = DebugLexerConsumer.new LEXER.lex(input), STDERR
+      STDERR.puts
+    {% end %}
 
-  program = Parser.new(LEXER.lex(input)).parse_program
+    puts String.build { |sb| StyleWalker.new LEXER.lex(input), sb }
 
-  {% if flag?("debug_ast") %}
-    PrettyPrint.format(program, STDOUT, 119)
-    puts "\n\n"
-  {% end %}
+    program = Parser.new(LEXER.lex(input)).parse_program
 
-  compiler = Compiler.new program
+    {% if flag?("debug_ast") %}
+      PrettyPrint.format(program, STDOUT, 119)
+      puts "\n\n"
+    {% end %}
+
+    program
+  end
+
+  compiler = Compiler.new source_programs
   File.open("out/out.ll", "w", &.puts(compiler.@mod.to_s))
   `llc out/out.ll -o out/out.s -O3`
   `clang out/out.s -static -o out/yume.out`
