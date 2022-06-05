45b2c264472c3937fd817ca78d340dbafa1a142b
Emilia Dreamer <emilia@rymiel.space>
2022-06-05 16:26:47 +0300

    this was redundant

--- src/compiler/type_walker.cpp
+++ src/compiler/type_walker.cpp
@@ -3,25 +3,18 @@
 #include "compiler.hpp"
 
 namespace yume {
-static auto observe_type(TypeWalkVisitor& self, ast::Type& type) {
-  if (type.kind() == ast::QualTypeKind) {
-    self.visit(dynamic_cast<ast::QualType&>(type).base(), nullptr);
-  }
-  auto* resolved_type = &self.m_compiler.convert_type(type, self.m_current_fn->parent());
-  type.val_ty(resolved_type);
-}
-
 auto TypeWalkVisitor::visit(ast::AST& expr, [[maybe_unused]] const char* label) -> TypeWalkVisitor& {
   if (expr.kind() == ast::TypeNameKind) {
     auto& type_name_expr = dynamic_cast<ast::TypeName&>(expr);
     auto& type = type_name_expr.type();
     type.add_observer(&type_name_expr);
-    observe_type(*this, type);
+    visit(type, nullptr);
     return *this;
   }
   if (expr.kind() == ast::SimpleTypeKind || expr.kind() == ast::QualTypeKind || expr.kind() == ast::SelfTypeKind) {
-    observe_type(*this, dynamic_cast<ast::Type&>(expr));
-    return *this;
+    auto& type = dynamic_cast<ast::Type&>(expr);
+    auto* resolved_type = &m_compiler.convert_type(type, m_current_fn->parent());
+    type.val_ty(resolved_type);
   }
   expr.visit(*this);
   return *this;
