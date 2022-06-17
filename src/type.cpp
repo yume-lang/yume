126a68034036b778c33824446924633ec49c861d
Emilia Dreamer <emilia@rymiel.space>
2022-06-17 20:48:09 +0300

    *slightly* more logical type compatibility heuristic

--- src/type.cpp
+++ src/type.cpp
@@ -77,14 +77,11 @@ auto Type::compatibility(const Type& other) const -> Compatiblity {
     }
     return {0};
   }
-  if (is_mut() && !other.is_mut() && qual_base() == &other) {
-    return {SAFE_CONVERSION};
-  }
-  if (is_scope() && !other.is_scope() && (qual_base() == other.qual_base() || qual_base() == &other)) {
-    return {SAFE_CONVERSION};
-  }
-  if (is_scope() && other.is_mut() && qual_base() == other.qual_base()) {
-    return {SAFE_CONVERSION};
+  if ((is_scope() && !other.is_scope()) || (is_mut() && !other.is_mut()) || (is_scope() && other.is_mut())) {
+    auto base_compatibility = qual_base()->compatibility(other.without_qual());
+    if (base_compatibility.rating != Compatiblity::INVALID) {
+      return base_compatibility;
+    }
   }
   if (const auto this_int = dyn_cast<Int>(this), other_int = dyn_cast<Int>(&other);
       (this_int != nullptr) && (other_int != nullptr)) {
