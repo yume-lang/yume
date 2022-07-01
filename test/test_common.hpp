dc8ee8ab9842ce41b95b95d490c7e485a78a9a57
Emilia Dreamer <emilia@rymiel.space>
2022-07-01 14:26:01 +0300

    Section off existing tests

--- test/test_common.hpp
+++ test/test_common.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "ast/ast.hpp"
+#include "diagnostic/print_visitor.hpp"
 #include "token.hpp"
 #include <catch2/matchers/catch_matchers_templated.hpp>
 #include <llvm/Support/raw_os_ostream.h>
@@ -19,19 +21,23 @@ struct EqualsRangeMatcher : Catch::Matchers::MatcherGenericBase {
     return std::equal(begin(range), end(range), begin(other), end(other), Pred{});
   }
 
-  auto describe() const -> std::string override { return "== " + Catch::rangeToString(range); }
+  auto describe() const -> std::string override { return "== " + ::Catch::rangeToString(range); }
 
 private:
   Range range;
 };
 
-static constexpr auto token_comparison = [](const yume::Token& a, const yume::Token& b) -> bool {
-  return a.m_type == b.m_type && a.m_payload == b.m_payload;
-};
+template <typename T, typename Pred = std::ranges::equal_to, typename Str = std::identity>
+struct EqualsDirectMatcher : Catch::Matchers::MatcherGenericBase {
+  EqualsDirectMatcher(T base) : base{std::move(base)} {}
 
-template <typename... Ts> auto EqualsTokens(Ts... ts) {
-  return EqualsRangeMatcher<std::vector<yume::Token>, decltype(token_comparison)>{{ts...}};
-}
+  template <typename U> auto match(const U& other) const -> bool { return Pred{}(other, base); }
+
+  auto describe() const -> std::string override { return "== " + ::Catch::Detail::stringify(Str{}(base)); }
+
+private:
+  T base;
+};
 
 template <typename T>
 concept os_adaptable = requires(llvm::raw_ostream& t_os, const T& t_obj) {
@@ -60,6 +66,18 @@ namespace Catch {
         ss.write_escaped(std::string(*token.m_payload));
         ss << "\")";
       }
+      return str;
+    }
+  };
+
+  template <typename T>
+  requires std::derived_from<T, yume::ast::AST>
+  struct StringMaker<T> {
+    static auto convert(const T& token) -> std::string {
+      std::string str = "(";
+      llvm::raw_string_ostream ss(str);
+      yume::diagnostic::PrintVisitor visitor(ss);
+      visitor.visit(token, nullptr);
       return str;
     }
   };
--- test/tokenizer_test.cpp
+++ test/tokenizer_test.cpp
@@ -4,6 +4,14 @@
 #include <catch2/catch_test_macros.hpp>
 
 namespace {
+constexpr auto token_comparison = [](const yume::Token& a, const yume::Token& b) -> bool {
+  return a.m_type == b.m_type && a.m_payload == b.m_payload;
+};
+
+template <typename... Ts> auto EqualsTokens(Ts... ts) {
+  return EqualsRangeMatcher<std::vector<yume::Token>, decltype(token_comparison)>{{ts...}};
+}
+
 using yume::make_atom;
 using yume::Token;
 using yume::tokenize;
@@ -31,50 +39,64 @@ template <bool FilterSkip> auto tkn(const std::string& str) -> std::vector<Token
 #define CHECK_TOKENIZER(...) CHECK_TOKENIZER_GEN(<1>, __VA_ARGS__)
 #define CHECK_TOKENIZER_PRESERVED(...) CHECK_TOKENIZER_GEN(<0>, __VA_ARGS__)
 
-TEST_CASE("Tokenization") {
+TEST_CASE("Tokenization", "[token]") {
   using enum yume::Token::Type;
 
-  CHECK_TOKENIZER("");
+  SECTION("Whitespace") {
+    CHECK_TOKENIZER("");
 
-  CHECK_TOKENIZER(" ");
-  CHECK_TOKENIZER_PRESERVED(" ", " "_Skip);
+    CHECK_TOKENIZER(" ");
+    CHECK_TOKENIZER_PRESERVED(" ", " "_Skip);
 
-  CHECK_TOKENIZER("\n", "\n"_Separator);
-
-  CHECK_TOKENIZER("hello", "hello"_Word);
-  CHECK_TOKENIZER("true", "true"_Word);
-  CHECK_TOKENIZER("false", "false"_Word);
-
-  CHECK_TOKENIZER("a b", "a"_Word, "b"_Word);
-  CHECK_TOKENIZER_PRESERVED("a b", "a"_Word, " "_Skip, "b"_Word);
-  CHECK_TOKENIZER("a\nb", "a"_Word, "\n"_Separator, "b"_Word);
-  CHECK_TOKENIZER("a\n  b", "a"_Word, "\n"_Separator, "b"_Word);
-
-  CHECK_TOKENIZER("# goodbye");
-  CHECK_TOKENIZER("# goodbye\n", "\n"_Separator);
-  CHECK_TOKENIZER("# goodbye\na", "\n"_Separator, "a"_Word);
-  CHECK_TOKENIZER_PRESERVED("# goodbye", "# goodbye"_Skip);
-  CHECK_TOKENIZER_PRESERVED("# goodbye\n", "# goodbye"_Skip, "\n"_Separator);
-  CHECK_TOKENIZER_PRESERVED("# goodbye\na", "# goodbye"_Skip, "\n"_Separator, "a"_Word);
-
-  CHECK_TOKENIZER("0", "0"_Number);
-  CHECK_TOKENIZER("123", "123"_Number);
-
-  CHECK_TOKENIZER("?a", "a"_Char);
-  CHECK_TOKENIZER("??", "?"_Char);
-  CHECK_TOKENIZER("?\\0", "\0"_Char);
-  CHECK_TOKENIZER("?\\\\", "\\"_Char);
-
-  CHECK_TOKENIZER(R"("hi")", R"(hi)"_Literal);
-  CHECK_TOKENIZER(R"("h\"i")", R"(h"i)"_Literal);
-  CHECK_TOKENIZER(R"("h\\i")", R"(h\i)"_Literal);
-
-  for (std::string i :
-       {"=", "<", ">", "+", "-", "*", "/", "//", "(", ")", "==", "!=", "!", ",", ".", ":", "%", "[", "]"}) {
-    CHECK_TOKENIZER(i, Token(Symbol, make_atom(i)));
+    CHECK_TOKENIZER("\n", "\n"_Separator);
   }
 
-  CHECK_TOKENIZER("[]", "["_Symbol, "]"_Symbol);
+  SECTION("Literals") {
+    CHECK_TOKENIZER("hello", "hello"_Word);
+    CHECK_TOKENIZER("true", "true"_Word);
+    CHECK_TOKENIZER("false", "false"_Word);
+
+    CHECK_TOKENIZER("a b", "a"_Word, "b"_Word);
+    CHECK_TOKENIZER_PRESERVED("a b", "a"_Word, " "_Skip, "b"_Word);
+    CHECK_TOKENIZER("a\nb", "a"_Word, "\n"_Separator, "b"_Word);
+    CHECK_TOKENIZER("a\n  b", "a"_Word, "\n"_Separator, "b"_Word);
+  }
+
+  SECTION("Comments") {
+    CHECK_TOKENIZER("# goodbye");
+    CHECK_TOKENIZER("# goodbye\n", "\n"_Separator);
+    CHECK_TOKENIZER("# goodbye\na", "\n"_Separator, "a"_Word);
+    CHECK_TOKENIZER_PRESERVED("# goodbye", "# goodbye"_Skip);
+    CHECK_TOKENIZER_PRESERVED("# goodbye\n", "# goodbye"_Skip, "\n"_Separator);
+    CHECK_TOKENIZER_PRESERVED("# goodbye\na", "# goodbye"_Skip, "\n"_Separator, "a"_Word);
+  }
+
+  SECTION("Numbers") {
+    CHECK_TOKENIZER("0", "0"_Number);
+    CHECK_TOKENIZER("123", "123"_Number);
+  }
+
+  SECTION("Characters") {
+    CHECK_TOKENIZER("?a", "a"_Char);
+    CHECK_TOKENIZER("??", "?"_Char);
+    CHECK_TOKENIZER("?\\0", "\0"_Char);
+    CHECK_TOKENIZER("?\\\\", "\\"_Char);
+  }
+
+  SECTION("String literals") {
+    CHECK_TOKENIZER(R"("hi")", R"(hi)"_Literal);
+    CHECK_TOKENIZER(R"("h\"i")", R"(h"i)"_Literal);
+    CHECK_TOKENIZER(R"("h\\i")", R"(h\i)"_Literal);
+  }
+
+  SECTION("Operators/symbols") {
+    for (std::string i :
+         {"=", "<", ">", "+", "-", "*", "/", "//", "(", ")", "==", "!=", "!", ",", ".", ":", "%", "[", "]"}) {
+      CHECK_TOKENIZER(i, Token(Symbol, make_atom(i)));
+    }
+
+    CHECK_TOKENIZER("[]", "["_Symbol, "]"_Symbol);
+  }
 }
 
 #undef CHECK_TOKENIZER_GEN
