db28b178c1f1755756dabf07699974986520e0d7
Emilia Dreamer <emilia@rymiel.space>
2022-07-03 02:43:08 +0300

    Refactor parser tests and add a few extras

--- test/parser_test.cpp
+++ test/parser_test.cpp
@@ -28,10 +28,16 @@ template <typename T, typename... Ts> auto ptr_vec(Ts&&... ts) {
   return vec;
 }
 
+template <typename T = CallExpr, typename E = Expr, typename N, typename... Ts>
+auto make_call(N&& name, Ts&&... ts) -> std::unique_ptr<T> {
+  return ast<T>(std::forward<N>(name), ptr_vec<E>(std::forward<Ts>(ts)...));
+}
+
 auto operator""_Var(const char* str, size_t size) { return ast<VarExpr>(std::string{str, size}); }
 auto operator""_Num(unsigned long long val) { return ast<NumberExpr>(val); }
 auto operator""_String(const char* str, size_t size) { return ast<StringExpr>(std::string{str, size}); }
 auto operator""_Char(char chr) { return ast<CharExpr>(chr); }
+auto operator""_Type(const char* str, size_t size) { return ast<SimpleType>(std::string{str, size}); }
 
 constexpr auto ast_comparison = [](const yume::ast::Program& a,
                                    const std::vector<std::unique_ptr<yume::ast::AST>>& b) -> bool {
@@ -61,10 +67,12 @@ template <typename... Ts> auto EqualsAST(Ts&&... ts) {
 } // namespace
 
 #define CHECK_PARSER(body, ...) CHECK_THAT(*prog(body), EqualsAST(__VA_ARGS__))
+#define CHECK_PARSER_THROWS(body) CHECK_THROWS(*prog(body))
 
-TEST_CASE("Parsing", "[parse]") {
-  using namespace yume::ast;
+using namespace yume::ast;
+using enum yume::Qualifier;
 
+TEST_CASE("Parse literals", "[parse]") {
   CHECK_PARSER("true", ast<BoolExpr>(true));
   CHECK_PARSER("false", ast<BoolExpr>(false));
 
@@ -73,24 +81,87 @@ TEST_CASE("Parsing", "[parse]") {
   CHECK_PARSER("?a", 'a'_Char);
 
   CHECK_PARSER(R"("hi")", "hi"_String);
+}
 
-  CHECK_PARSER("1 + 2", ast<CallExpr>("+", ptr_vec<Expr>(1_Num, 2_Num)));
-  CHECK_PARSER("1 +2", ast<CallExpr>("+", ptr_vec<Expr>(1_Num, 2_Num)));
-  CHECK_PARSER("1 - 2", ast<CallExpr>("-", ptr_vec<Expr>(1_Num, 2_Num)));
-  CHECK_PARSER("1 -2", ast<CallExpr>("-", ptr_vec<Expr>(1_Num, 2_Num)));
-  CHECK_PARSER("-2", ast<CallExpr>("-", ptr_vec<Expr>(2_Num)));
-  CHECK_PARSER("!true", ast<CallExpr>("!", ptr_vec<Expr>(ast<BoolExpr>(true))));
+TEST_CASE("Parse operator calls", "[parse]") {
+  CHECK_PARSER("1 + 2", make_call("+", 1_Num, 2_Num));
+  CHECK_PARSER("1 +2", make_call("+", 1_Num, 2_Num));
+  CHECK_PARSER("1 - 2", make_call("-", 1_Num, 2_Num));
+  CHECK_PARSER("1 -2", make_call("-", 1_Num, 2_Num));
+  CHECK_PARSER("-2", make_call("-", 2_Num));
+  CHECK_PARSER("!true", make_call("!", ast<BoolExpr>(true)));
+}
 
+TEST_CASE("Parse operator precedence", "[parse]") {
+  CHECK_PARSER("1 + 2 * 3 == 6 + 1",
+               make_call("==", make_call("+", 1_Num, make_call("*", 2_Num, 3_Num)), make_call("+", 6_Num, 1_Num)));
+}
+
+TEST_CASE("Parse assignment", "[parse]") {
   CHECK_PARSER("a = 1\nb = 2", ast<AssignExpr>("a"_Var, 1_Num), ast<AssignExpr>("b"_Var, 2_Num));
   CHECK_PARSER("a = b = 2", ast<AssignExpr>("a"_Var, ast<AssignExpr>("b"_Var, 2_Num)));
+}
 
+TEST_CASE("Parse direct calling", "[parse]") {
   CHECK_PARSER("a", "a"_Var);
   CHECK_PARSER("(a)", "a"_Var);
-  CHECK_PARSER("a()", ast<CallExpr>("a", ptr_vec<Expr>()));
-  // CHECK_PARSER("(a)()", ast<CallExpr>("a", ptr_vec<Expr>()));
+  CHECK_PARSER("a()", make_call("a"));
+  CHECK_PARSER("a(1, 2, 3)", make_call("a", 1_Num, 2_Num, 3_Num));
+  CHECK_PARSER("a(b(1), c(2, 3))", make_call("a", make_call("b", 1_Num), make_call("c", 2_Num, 3_Num)));
+}
 
-  CHECK_PARSER("a[b]", ast<CallExpr>("[]", ptr_vec<Expr>("a"_Var, "b"_Var)));
-  CHECK_PARSER("a[b] = c", ast<CallExpr>("[]=", ptr_vec<Expr>("a"_Var, "b"_Var, "c"_Var)));
+// TODO:
+TEST_CASE("Parse indirect calling", "[!shouldfail][parse]") { CHECK_PARSER("(a)()", make_call("a")); }
+
+TEST_CASE("Parse member calling", "[parse]") {
+  CHECK_PARSER("receiver.call", make_call("call", "receiver"_Var));
+  CHECK_PARSER("receiver.call()", make_call("call", "receiver"_Var));
+  CHECK_PARSER("receiver.call(1, 2, 3)", make_call("call", "receiver"_Var, 1_Num, 2_Num, 3_Num));
+}
+
+TEST_CASE("Parse setter calling", "[parse]") {
+  CHECK_PARSER("receiver.foo = 0", make_call("foo=", "receiver"_Var, 0_Num));
+  CHECK_PARSER("r.f = r", make_call("f=", "r"_Var, "r"_Var));
+}
+
+TEST_CASE("Parse direct field access", "[parse]") {
+  CHECK_PARSER("widget::field", ast<FieldAccessExpr>("widget"_Var, "field"));
+  CHECK_PARSER("widget::field.run", make_call("run", ast<FieldAccessExpr>("widget"_Var, "field")));
+
+  CHECK_PARSER("widget::field = 0", ast<AssignExpr>(ast<FieldAccessExpr>("widget"_Var, "field"), 0_Num));
+}
+
+TEST_CASE("Parse constructor calling", "[parse]") {
+  CHECK_PARSER("Type()", make_call<CtorExpr>("Type"_Type));
+  CHECK_PARSER("Type(a)", make_call<CtorExpr>("Type"_Type, "a"_Var));
+
+  CHECK_PARSER("Type[]()", make_call<CtorExpr>(ast<QualType>("Type"_Type, Slice)));
+  CHECK_PARSER("Type[](a)", make_call<CtorExpr>(ast<QualType>("Type"_Type, Slice), "a"_Var));
+}
+
+TEST_CASE("Parse slice literal", "[parse]") {
+  CHECK_PARSER("I32[1, 2, 3]", make_call<SliceExpr>("I32"_Type, 1_Num, 2_Num, 3_Num));
+}
+
+// TODO:
+TEST_CASE("Parse empty slice literal", "[!shouldfail][parse]") {
+  CHECK_PARSER("I32[]", make_call<SliceExpr>("I32"_Type));
+}
+
+TEST_CASE("Parse bare type", "[parse][throws]") {
+  CHECK_PARSER_THROWS("T ptr");
+  CHECK_PARSER_THROWS("I32");
+}
+
+TEST_CASE("Parse index operator", "[parse]") {
+  CHECK_PARSER("a[b]", make_call("[]", "a"_Var, "b"_Var));
+  CHECK_PARSER("a[b] = c", make_call("[]=", "a"_Var, "b"_Var, "c"_Var));
+}
+
+TEST_CASE("Parse variable declaration", "[parse]") {
+  CHECK_PARSER("let a = 0", ast<VarDecl>("a", std::nullopt, 0_Num));
+  CHECK_PARSER("let a I32 = 0", ast<VarDecl>("a", "I32"_Type, 0_Num));
 }
 
 #undef CHECK_PARSER
+#undef CHECK_PARSER_THROWS
