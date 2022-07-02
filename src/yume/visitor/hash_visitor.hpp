d7d727ab2d0537535a994afaeb8f657c28e368e7
Emilia Dreamer <emilia@rymiel.space>
2022-07-02 22:21:14 +0300

    hash_append with fewer magic numbers

--- src/yume/visitor/hash_visitor.hpp
+++ src/yume/visitor/hash_visitor.hpp
@@ -3,6 +3,8 @@
 #include "util.hpp"
 #include "visitor.hpp"
 #include <iosfwd>
+#include <limits>
+#include <numbers>
 #include <string>
 
 namespace yume::ast {
@@ -17,8 +19,15 @@ class HashVisitor : public Visitor {
   uint64_t& m_seed;
 
   template <typename T> static void hash_combine(uint64_t& seed, const T& v) {
+    static constexpr auto PHI_FRAC = std::numbers::phi_v<long double> - 1;
+    static constexpr auto ALL_BITS = std::numeric_limits<std::size_t>::max();
+    static constexpr auto FLOATING_HASH_CONST = PHI_FRAC * ALL_BITS;
+    static constexpr auto HASH_CONST = static_cast<std::size_t>(FLOATING_HASH_CONST);
+    static constexpr auto TWIST_LEFT = 6;
+    static constexpr auto TWIST_RIGHT = 2;
+
     std::hash<T> hasher;
-    seed ^= hasher(v) + 0x9e3779b97f4a7c15 + (seed << 6) + (seed >> 2); // NOLINT
+    seed ^= hasher(v) + HASH_CONST + (seed << TWIST_LEFT) + (seed >> TWIST_RIGHT);
   }
 
 public:
