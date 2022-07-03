96774ed7a69edac41a48904fca1bbd5a767ba6db
Emilia Dreamer <emilia@rymiel.space>
2022-07-03 03:44:10 +0300

    Clean up ci script and publish test results as junit

--- bin/ci.sh
+++ bin/ci.sh
@@ -2,24 +2,22 @@
 
 case $1 in
   "examples" )
-    enabled_examples=(
-      "bf"
-      "collatz"
-      "fizzbuzz"
-      "generic"
-      "overloads"
-      "slice"
-      "struct"
-    )
-
-    for i in ${enabled_examples[@]}; do
+    for i in example/*; do
+      [[ -f "${i}" ]] || continue
       echo "Running example ${i}.ym"
-      build/yumec example/${i}.ym
-      mv output.ll result-${i}.ll
+      build/yumec example/"${i}".ym
+      mv output.ll result-"${i}".ll
       ./yume.out
     done;;
 
+  "test" )
+    cd ./build || exit
+    ./yume_test -r junit::out=junit.xml -r console;;
+
   "coverage" )
     llvm-profdata-13 merge -sparse default.profraw -o default.profdata
-    llvm-cov-13 show ./yume_test -instr-profile default.profdata -Xdemangler c++filt -Xdemangler -i -format html -output-dir cov -ignore-filename-regex '/(test|usr)/.*'
+    llvm-cov-13 show ./yume_test -instr-profile default.profdata -Xdemangler c++filt -Xdemangler -i -format html -output-dir cov -ignore-filename-regex '/(test|usr)/.*';;
+
+  *)
+    echo "Invalid usage"
 esac
