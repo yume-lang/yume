a8f21987c288dceae100f91d94d5da9603455674
Emilia Dreamer <emilia@rymiel.space>
2022-07-03 15:44:33 +0300

    Extract ast related login in template instantiation into Fn

--- src/yume/compiler/vals.cpp
+++ src/yume/compiler/vals.cpp
@@ -9,4 +9,17 @@ auto Fn::declaration(Compiler& compiler, bool mangle) -> llvm::Function* {
   }
   return m_llvm_fn;
 }
+
+auto Fn::create_template_instantiation(Instantiation& instantiate) -> Fn& {
+  auto* decl_clone = m_ast_decl.clone();
+  m_member->direct_body().emplace_back(decl_clone);
+
+  std::map<string, const ty::Type*> subs{};
+  for (const auto& [k, v] : instantiate.sub)
+    subs.try_emplace(k->name(), v);
+
+  auto fn_ptr = std::make_unique<Fn>(*decl_clone, m_parent, m_member, move(subs));
+  auto new_emplace = m_instantiations.emplace(instantiate, move(fn_ptr));
+  return *new_emplace.first->second;
+}
 } // namespace yume
