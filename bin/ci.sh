72e1f075740c5c7d2441beed5a1eb6c00a688c4e
Emilia Dreamer <emilia@rymiel.space>
2022-07-03 04:02:55 +0300

    Resolve issues with build dir in CI

--- bin/ci.sh
+++ bin/ci.sh
@@ -1,20 +1,24 @@
 #!/usr/bin/env bash
 
+declare BUILD_DIR
+
 case $1 in
   "examples" )
     for i in example/*; do
       [[ -f "${i}" ]] || continue
       echo "Running example ${i}.ym"
-      build/yumec example/"${i}".ym
-      mv output.ll result-"${i}".ll
+      "${BUILD_DIR}"/yumec "${i}" || exit 1
+      mv output.ll result-"$(basename "${i}")".ll
+      rm output.ll
       ./yume.out
     done;;
 
   "test" )
-    cd ./build || exit
+    cd "${BUILD_DIR}" || exit
     ./yume_test -r junit::out=junit.xml -r console;;
 
   "coverage" )
+    cd "${BUILD_DIR}" || exit
     llvm-profdata-13 merge -sparse default.profraw -o default.profdata
     llvm-cov-13 show ./yume_test -instr-profile default.profdata -Xdemangler c++filt -Xdemangler -i -format html -output-dir cov -ignore-filename-regex '/(test|usr)/.*';;
 
