b92b9c33c051f11d151a68b8fd69c729dd4d1e7c
Emilia Dreamer <emilia@rymiel.space>
2022-07-04 22:18:00 +0300

    oops

--- bin/ci.sh
+++ bin/ci.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 declare BUILD_DIR
+declare LLVM_PROFDATA
+declare LLVM_COV
 
 case $1 in
   "examples" )
@@ -18,8 +20,8 @@ case $1 in
 
   "coverage" )
     cd "${BUILD_DIR}" || exit
-    llvm-profdata-13 merge -sparse default.profraw -o default.profdata
-    llvm-cov-13 show ./yume_test -instr-profile default.profdata -Xdemangler c++filt -Xdemangler -i -format html -output-dir cov -ignore-filename-regex '/(test|usr)/.*';;
+    ${LLVM_PROFDATA} merge -sparse default.profraw -o default.profdata
+    ${LLVM_COV} show ./yume_test -instr-profile default.profdata -Xdemangler c++filt -Xdemangler -i -format html -output-dir cov -ignore-filename-regex '/(test|usr)/.*';;
 
   *)
     echo "Invalid usage"
