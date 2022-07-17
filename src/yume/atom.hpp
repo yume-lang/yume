16a2f6a0e3d0f2e2bcb9521097cc64399b42b8aa
Emilia Dreamer <emilia@rymiel.space>
2022-07-17 05:45:08 +0300

    refactor: make Atom use llvm datatypes

--- src/yume/atom.hpp
+++ src/yume/atom.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-#include <set>
+#include "llvm/ADT/StringSet.h"
 #include <string>
 
 namespace yume {
@@ -10,31 +10,32 @@ namespace yume {
  * equality check.
  */
 class Atom {
-  const std::string* m_str;
+  std::string_view m_str;
 
 public:
   constexpr Atom() = delete;
-  explicit Atom(const std::string* str) : m_str{str} {}
+  explicit constexpr Atom(std::string_view str) : m_str{str} {}
 
-  /* implicit */ operator std::string_view() const { return *m_str; }
-  auto operator<=>(const Atom& other) const noexcept = default;
+  /* implicit */ operator std::string_view() const { return m_str; }
+  explicit operator std::string() const { return std::string(m_str); }
+  auto constexpr operator<=>(const Atom& other) const noexcept = default;
 
-  static auto inline make_atom(const std::string& value) noexcept -> Atom {
-    auto data = Atom::interned.emplace(value).first;
-    return Atom{&*data};
+  static auto inline make_atom(std::string_view value) noexcept -> Atom {
+    const auto& data = Atom::interned.insert(value).first->first();
+    return Atom{data};
   }
 
 private:
-  static inline std::set<std::string> interned; // NOLINT(cppcoreguidelines-avoid-non-const-global-variables)
+  static inline llvm::StringSet interned{}; // NOLINT(cppcoreguidelines-avoid-non-const-global-variables)
 };
 
 // using Atom = const char*;
 
 /// Create an `Atom` with the given string content.
 /// \sa atom_literal::operator""_a
-auto inline make_atom(const std::string& value) noexcept -> Atom { return Atom::make_atom(value); }
+auto inline make_atom(std::string_view value) noexcept -> Atom { return Atom::make_atom(value); }
 
 auto inline operator""_a(const char* value, std::size_t len) noexcept -> Atom {
-  return make_atom(std::string(value, len));
+  return make_atom(std::string_view(value, len));
 }
 } // namespace yume
