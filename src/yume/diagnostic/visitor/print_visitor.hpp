a041a8decd2616523451945fe1bc8151664a66e8
Emilia Dreamer <emilia@rymiel.space>
2022-09-27 08:48:46 +0300

    Add indenting logic to PrintVisitor

--- src/yume/diagnostic/visitor/print_visitor.hpp
+++ src/yume/diagnostic/visitor/print_visitor.hpp
@@ -15,12 +15,16 @@ class raw_ostream;
 namespace yume::diagnostic {
 class PrintVisitor : public Visitor {
   llvm::raw_ostream& m_stream;
+  bool m_pretty;
   bool m_needs_sep = false;
+  int m_indent = 0;
 
   void header(string_view label);
+  void indent(string_view text);
+  void dedent(string_view text);
 
 public:
-  explicit PrintVisitor(llvm::raw_ostream& stream) : m_stream{stream} {}
+  explicit PrintVisitor(llvm::raw_ostream& stream, bool pretty = false) : m_stream{stream}, m_pretty(pretty) {}
   ~PrintVisitor() override = default;
 
   PrintVisitor(const PrintVisitor&) = delete;
@@ -35,12 +39,24 @@ public:
 
 inline void PrintVisitor::header(string_view label) {
   if (m_needs_sep)
-    m_stream << " ";
+    m_stream << (m_pretty ? ("\n" + std::string(m_indent * 2UL, ' ')) : " ");
   else
     m_needs_sep = true;
 
   if (!label.empty())
     m_stream << label << "=";
+}
+
+inline void PrintVisitor::indent(string_view text) {
+  m_stream << text;
+  ++m_indent;
+  m_stream << std::string(m_indent * 2UL, ' ');
+}
+
+inline void PrintVisitor::dedent(string_view text) {
+  m_stream << text;
+  --m_indent;
+  m_stream << std::string(m_indent * 2UL, ' ');
 }
 
 inline auto PrintVisitor::visit(const ast::AST& expr, string_view label) -> PrintVisitor& {
@@ -49,9 +65,13 @@ inline auto PrintVisitor::visit(const ast::AST& expr, string_view label) -> Prin
   m_stream.changeColor(llvm::raw_ostream::SAVEDCOLOR, true) << expr.kind_name();
   m_stream.resetColor();
   m_stream << "(";
+  if (m_pretty)
+    indent("\n");
   m_needs_sep = false;
 
   expr.visit(*this);
+  if (m_pretty)
+    dedent("\n");
   m_stream << ")";
 
   return *this;
