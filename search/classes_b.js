3386720ea83dff93a9031325d3a8bb044710f61c
rymiel <rymiel@users.noreply.github.com>
2022-10-08 11:49:17 +0000

    Deploying to gh-pages from @ yume-lang/yume@bd3bfcebbad08e9f968ce8dbdea33ac665983c6d 🚀

--- search/classes_b.js
+++ search/classes_b.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['numberexpr_0',['NumberExpr',['../structyume_1_1ast_1_1NumberExpr.html',1,'yume::ast']]]
+  ['nil_0',['Nil',['../classyume_1_1ty_1_1Nil.html',1,'yume::ty']]],
+  ['numberexpr_1',['NumberExpr',['../structyume_1_1ast_1_1NumberExpr.html',1,'yume::ast']]]
 ];
